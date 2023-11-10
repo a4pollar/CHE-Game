57ae98defcaf2fc22e0378614f74f32dfbdfa0ca
a2jakana <149831727+a2jakana@users.noreply.github.com>
2023-11-10 11:51:57 -0500

    Update test.py

--- test.py
+++ test.py
@@ -1,2 +1,13 @@
 print("My name is Alexa.")
 print("My PRONOUNS ARE U. S. A. RAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAH")
+
+def penis(dickless):
+  if type(dickless) == str:
+    print("AHHH MY PENIS")
+    return None
+  else:
+    print("invalid input")
+    return None
+    
+
+    
